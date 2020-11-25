b283833c8d060266c5268d3fe99a42ceebef85b7
Jordi Boix Baró <jordi_nit@msn.com>
2020-11-26 00:09:22 +0100

    back to previous version

--- src/android/OpenTokAndroidPlugin.java
+++ src/android/OpenTokAndroidPlugin.java
@@ -113,7 +113,7 @@ public class OpenTokAndroidPlugin extends CordovaPlugin
 
         public void updateZIndices() {             
             
-           // if(subscriberCollection.isEmpty()) return; 
+            if(subscriberCollection.isEmpty()) return; 
 
             allStreamViews = new ArrayList<RunnableUpdateViews>();
             for (Map.Entry<String, RunnableSubscriber> entry : subscriberCollection.entrySet()) {
